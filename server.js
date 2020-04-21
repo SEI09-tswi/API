1fb1b348b85905240eefa4bb31e9318c9e486cf5
YouriFrancois <yourifrancois97@gmail.com>
2020-04-21 11:31:11 -0400

    we are able to create new message from different user
    Co-authored-by: Jennifer <Jenniferpayano@gmail.com>
    Co-authored-by: Murat  <alayurtmm@gmail.com>
    Co-authored-by: nick  <nick.solie1@gmail.com>

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 // require route files
 const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
+const chatroomRoutes = require('./app/routes/chatroom_routes')
 
 // require middleware
 const errorHandler = require('./lib/error_handler')
@@ -63,6 +64,8 @@ app.use(requestLogger)
 // register route files
 app.use(exampleRoutes)
 app.use(userRoutes)
+app.use(chatroomRoutes)
+
 
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
